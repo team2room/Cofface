7f30d3101d73659774a7e51796a5280ff3c6a87f
권규리 <kyuri4809@naver.com>
2025-04-30 17:48:54 +0900

    [#S12P31E202-94][FE] Design: 관리자 로그인 화면 구현 중

--- FE/kiosk/src/App.tsx
+++ FE/kiosk/src/App.tsx
@@ -1,43 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-import tw from 'twin.macro'
-import styled from '@emotion/styled'
-
-// Tailwind 클래스를 styled-components처럼 사용 가능
-const Title = styled.h1`
-  ${tw`text-3xl font-bold text-blue-600`}
-`
+import { Route, Routes } from 'react-router-dom'
+import Fonts from './styles/fonts'
+import AdminLoginPage from './pages/adminLogin/AdminLoginPage'
+import 'react-simple-keyboard/build/css/index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <Title>Welcome to My PWA ddd</Title>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Fonts />
+      <Routes>
+        <Route path="/" element={<AdminLoginPage />} />
+      </Routes>
     </>
   )
 }
