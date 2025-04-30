ade251b285dbf6c271ff367b8a26692870c5f04e
권규리 <kyuri4809@naver.com>
2025-04-30 14:41:17 +0900

    [#S12P31E202-62][FE] Design: 전체 폰트 사이즈 및 색상 상수 설정

---
+++ FE/kiosk/src/styles/colors.ts
@@ -0,0 +1,33 @@
+export const colors = {
+  // text
+  lightLight: '#FFF7FA',
+  light: '#FFF0F6',
+  littleLight: '#FEE0EC',
+  main: '#E0115F',
+  littleDark: '#CB1257',
+  dark: '#AD124D',
+  darkDark: '#772745',
+
+  lightYellow: '#FFF8EC',
+  lightOrange: '#FFF1EF',
+
+  white: '#FFFFFF',
+  littleGray: '#F4F4F4',
+  gray: '#D9D9D9',
+  littleDarkGray: '#B7B7B7',
+  darkGray: '#6D6D6D',
+  lightBlack: '#434343',
+  black: '#000000',
+
+  // background
+  modal: 'rgba(0, 0, 0, 0.5)',
+  lightModal: 'rgba(0, 0, 0, 0.3)',
+
+  // button
+  default: '#E0115F',
+  hover: '#FF2175',
+  disabled: '#FAD4E3',
+  pressed: '#CB1257',
+} as const
+
+export type ColorName = keyof typeof colors
