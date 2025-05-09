ef68a451db1511a7d457e2293c2c1a0e0ed4fa3c
이혜령 <dlgofud123@naver.com>
2025-05-09 11:19:27 +0900

    [#S12P31E202-150][FE] Feat: 얼굴 등록 화면 디자인 수정 중

--- FE/user-app/src/pages/register/FaceRegisterCapturePage.tsx
+++ FE/user-app/src/pages/register/FaceRegisterCapturePage.tsx
@@ -1,9 +1,20 @@
 import tw from 'twin.macro'
 
 const Container = tw.div`
-  w-full max-w-screen-sm mx-auto flex flex-col min-h-screen
+  w-full max-w-screen-sm mx-auto flex flex-col min-h-screen bg-black
+`
+const HeaderWrapper = tw.div`
+  sticky top-0 z-10 bg-black w-full
+`
+const ContentWrapper = tw.div`
+  flex flex-col px-6 flex-1 pb-6
 `
 
 export function FaceRegisterCapturePage() {
-  return <Container></Container>
+  return (
+    <Container>
+      <HeaderWrapper></HeaderWrapper>
+      <ContentWrapper></ContentWrapper>
+    </Container>
+  )
 }
