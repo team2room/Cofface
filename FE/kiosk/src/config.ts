c28bc40b3d387a93fb7ff63627167f2c94a64c0b
권규리 <kyuri4809@naver.com>
2025-05-10 23:48:03 +0900

    [#S12P31E202-98][FE] Feat: 키오스크 관리자 로그인 api 연결 완료

--- FE/kiosk/src/config.ts
+++ FE/kiosk/src/config.ts
@@ -1,2 +1,1 @@
-export const BASE_URL = process.env.VITE_BASE_URL || 'http://localhost:8000'
-// export const BASE_URL = import.meta.env.VITE_BASE_URL
+export const BASE_URL = import.meta.env.VITE_BASE_URL
