2e9b3a36035a7f65d3968ee152bffd16d36f5570
박혜원 <hyewon5342@nate.com>
2025-05-15 14:20:38 +0900

    [#S12P31E202-83][BE] Fix: 시큐리티 설정 변경

--- BE/orderme/src/main/java/com/ssafy/orderme/config/SecurityConfig.java
+++ BE/orderme/src/main/java/com/ssafy/orderme/config/SecurityConfig.java
@@ -35,17 +35,17 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http, JwtTokenProvider jwtTokenProvider) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth/verify/**").permitAll()
                         .requestMatchers("/api/auth/admin/**").permitAll()
-                        .requestMatchers("/api/kiosk/**").permitAll()
-                        .requestMatchers("/api/admin/**").permitAll()
                         .requestMatchers("/api/auth/kiosk/phone-login").permitAll()
                         .requestMatchers("/api/auth/kiosk/face-login").permitAll()
                         .requestMatchers("/api/auth/refresh").permitAll()
                         .requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/api/payments/**").permitAll()
+                        .requestMatchers("/api/kiosk/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 // 필터를 직접 생성하여 추가
@@ -98,4 +98,3 @@ public class SecurityConfig {
         return source;
     }
 }
-
